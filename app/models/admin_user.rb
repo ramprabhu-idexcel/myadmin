7b8b9f278665ab5726a5cf04b1ac01d64a80f328
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-30 17:58:13 +0530

    Secure token

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,7 @@ class AdminUser < ApplicationRecord
   self.table_name = "users"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_secure_token
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
   include Mygem::UserScope

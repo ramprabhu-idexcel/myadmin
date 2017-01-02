ec247e7371dcfd4ff867eb41caa3c6e0e3789abf
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-02 15:37:13 +0530

    restrict only admin users

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :first_name, :last_name, :username, :email, :age, :address, :phone, :password, :password_confirmation
+  permit_params :first_name, :last_name, :username, :email, :age, :address, :phone, :password, :password_confirmation, :is_admin
 
   index do
     selectable_column
@@ -10,6 +10,7 @@ ActiveAdmin.register AdminUser do
     column :age
     column :address
     column :phone
+    column :is_admin
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -24,12 +25,14 @@ ActiveAdmin.register AdminUser do
   filter :address
   filter :phone
   filter :sign_in_count
+  filter :is_admin
 
   form do |f|
     f.inputs "Admin Details" do
       f.input :first_name
       f.input :last_name
       f.input :username
+      f.input :is_admin
       f.input :email
       f.input :password
       f.input :password_confirmation
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # restrict access to admin module for non-admin users
+  def authenticate_admin_user!
+    raise SecurityError unless current_admin_user.try(:is_admin?)
+  end
+
+  # path for redirection after user sign_in, depending on user role
+  def after_sign_in_path_for(user)
+    user.is_admin? ? admin_dashboard_path : root_path
+  end
+
+  # raise security error if access has been denied
+  rescue_from SecurityError do |exception|
+    sign_out
+    redirect_to new_admin_user_session_path
+  end
 end

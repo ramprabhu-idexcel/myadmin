1fda376df91d818498c307defd4ba01fea42322f
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-06 12:52:11 +0530

    pg table name issue

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -23,7 +23,7 @@ ActiveAdmin.register_page "Dashboard" do
                                 ORDER BY table_name ASC;")
 
         all_models_count = records.collect do |record|
-          [record["table_name"], ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM #{record["table_name"]};").to_i]
+          [record["table_name"], ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM #{record['table_name']};")]
         end
 
         puts "ALL MODELS COUNT"
